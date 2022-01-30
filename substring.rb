91d8ff008067ad612297953b866cc8bc5b2857e8
Cory <co.ry@icloud.com>
2022-01-30 17:24:19 -0500

    map method on dictionary

--- substring.rb
+++ substring.rb
@@ -1,1 +1,8 @@
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+def print_test(dictionary)
+   puts dictionary.map { |i| "'" + i.to_s + "'" }.join(",")
+   
+end
+
+print_test(dictionary)
