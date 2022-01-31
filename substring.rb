2ce97417b529b99e277c70283260afce109ae7a8
Cory <co.ry@icloud.com>
2022-01-30 20:43:39 -0500

    cleaned up map

--- substring.rb
+++ substring.rb
@@ -1,8 +1,8 @@
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
 def print_test(dictionary)
-   puts dictionary.map { |i| "'" + i.to_s + "'" }.join(",")
-   
+   puts dictionary.map {|i| i.to_s}
+
 end
 
 print_test(dictionary)
